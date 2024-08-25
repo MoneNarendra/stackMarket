ceef0149c35dd7782d150e8a722306f35b53879b
MoneNarendra <monenarendrakumar@gmail.com>
2024-08-26 00:23:48 +0530

    Added files

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <center>
+            <h1>Stock Market Prediction App</h1>
+        </center>
+    );
+}
+
+export default Header;
