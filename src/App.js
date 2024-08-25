b4c46d784640bdda000f3fcbd9a05ba225b26adf
MoneNarendra <monenarendrakumar@gmail.com>
2024-08-26 00:23:48 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Components/Header';
+import StockChart from './Components/StockChart';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <StockChart />
+        </div>
+    );
+}
+
+export default App;
